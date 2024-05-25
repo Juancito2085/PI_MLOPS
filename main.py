a58961f16cfa73bb23c2c155bd0e7f2157f67e17
Juancito2085 <jcbrunello@hotmail.com>
2024-05-25 12:10:37 -0300

    Primer endpoint terminado

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+import pandas as pd
 
 
 app = FastAPI()
 
-@app.get("/developer_reviews_analysis/{desarrollador}",description="Es una prubea")
+@app.get("/developer_reviews_analysis/{desarrollador}",description="Devuelve un diccionario con el nombre del desarrollador y la cantidad de reviews positivas y negativas")
 async def developer_reviews_analysis( desarrollador : str ):
-    return {"valve" : desarrollador*2}
+    desarrollador=desarrollador.title()
+    df=pd.read_parquet("Datasets/developers.parquet")
+    respuesta=df[df['developer']==desarrollador][['Positive','Negative']].sum()
+    respuesta = [f"{k} = {v}" for k, v in respuesta.items()]
+    if desarrollador not in df["developer"].values:
+        return {"respuesta" : "Desarrollador no encontrado"}
+    else:
+        return {"respuesta" : respuesta}
 
