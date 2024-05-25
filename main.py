dc4c1a49ceb3ed0ee261c2365f4e4dc2a6cf65e1
Juancito2085 <jcbrunello@hotmail.com>
2024-05-25 11:10:08 -0300

    Primera prueba

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/developer_reviews_analysis/{desarrolladora}")
+@app.get("/developer_reviews_analysis/{desarrolladora}",description="Es una prubea")
 async def developer_reviews_analysis( desarrolladora : str ):
     return {"valve" : {"Negative" : 182, "Neutral" : 120, "Positive" : 278}}
