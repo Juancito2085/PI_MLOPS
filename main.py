5495b5037ae264f11db17c54040154a98817dd49
Juancito2085 <jcbrunello@hotmail.com>
2024-05-26 19:39:44 -0300

    Prueba segundo punto

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
 
+#Funcion para los json
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
 #Funcion de recomendacion de juegos
 
 def similitud(id):
@@ -28,6 +39,29 @@ def similitud(id):
     return most_similar_products
 
 app = FastAPI()
+
+@app.get("/developer/{desarrollador}",description="Devuelve un diccionario con el nombre del desarrollador y la cantidad de items por año y la proporción de items gratis")
+async def developer( desarrollador : str ):
+    desarrollador=desarrollador.title()
+    df_developers=pd.read_parquet(r'Datasets/developers.parquet')
+    if desarrollador in df_developers['developer'].values:
+        df_developers=df_developers[df_developers['developer']==desarrollador]
+        #Se eliminan las columnas que no se van a utilizar
+        df_developers.drop(columns=['Negative','Neutral','Positive','True','False','developer'],inplace=True)
+        #Total de items por cada año
+        total = df_developers.groupby('release_year')['price'].count()
+        #Cuenta la cantidad de items que no son gratis
+        no_ceros = df_developers[df_developers['price'] != 0].groupby('release_year')['price'].count()
+        no_ceros= no_ceros.reindex(total.index, fill_value=0)
+        #Calacula la proporción de items gratis
+        proporcion_gratis =round((1- no_ceros / total)*100,2)
+        proporcion_gratis=proporcion_gratis.astype('str')+'%'
+        #Doy formato a la respuesta
+        data = [{'Año': int(year), 'Cantidad de items': int(total[year]), 'Contenido free': proporcion_gratis[year]} for year in total.index]
+        return{desarrollador:data}
+    else:
+        return{'No existe el desarrollador '+ desarrollador}
+
 
 @app.get("/developer_reviews_analysis/{desarrollador}",description="Devuelve un diccionario con el nombre del desarrollador y la cantidad de reviews positivas y negativas")
 async def developer_reviews_analysis( desarrollador : str ):
